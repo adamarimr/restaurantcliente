d7d55cd2241f93c29ee4d2d3667e1253564c9659
Adamari <adamari3939@gmail.com>
2023-05-28 23:54:22 -0400

    React Web App

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCeovf1cLsHS5PaobpULG6nAPMcrQfEKrU",
+    authDomain: "restaurant-5e37c.firebaseapp.com",
+    projectId: "restaurant-5e37c",
+    storageBucket: "restaurant-5e37c.appspot.com",
+    messagingSenderId: "14875979912",
+    appId: "1:14875979912:web:f4bb3b8354d3ce03b6ef3d",
+    measurementId: "G-M68VP3837D"
+  };
+
+export default firebaseConfig;
